14100c7fd313ccb2f6ecba69aded9bd5d55b481e
vodopadikcontur <40123638+vodopadikcontur@users.noreply.github.com>
2020-03-21 18:21:04 +0200

    last changing

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ function getData () {
     .then(res => res.json())
     .then(data => getBeer(data));
 }
-
+let ig = 0;
 function getBeer (data) {
   sessionStorage.beer = JSON.stringify(data);
-  console.log(data);
     for (let i=0; i < data.length; i ++) {
+      ig = i;
       let drawBeer = `<div class="col-12 col-sm-4 col-md-4">
         <div class="card-beer">
             <div class="product-img">
@@ -22,8 +22,8 @@ function getBeer (data) {
             <div class="product-content">
               <p class="product-title font-weight-bold">${data[i].name}</p>
               <p>${data[i].tagline}</p>
-              <p> <span id="plus" class="btn btn-light">+</span> <span id="minus" class="btn btn-light">-</span></p>
-              <p>Count - <span id="count" data-count="${i}">1</span></p>
+              <p> <button id="plus" class="btn btn-light">+</button> <button id="minus" class="btn btn-light">-</button></p>
+              <p>Count - <span id="count" data-count="${ig}">1</span></p>
               <button data-id="${data[i].id}" href="#" class="btn btn-light" data-toggle="modal" data-target="#exampleModal">Buy</button>
             </div>
             <div class="desc">
@@ -39,7 +39,6 @@ window.onclick = (event) => {
   let card = event.target.closest('.col-12');
   let plus = event.target.closest('#plus');
   let minus = event.target.closest('#minus');
-  //console.log(card);
   
   if(card) {
     let productId = event.target.getAttribute('data-id');
@@ -52,6 +51,9 @@ window.onclick = (event) => {
   }
   if(plus) sumCount(card, a = 1);
   if(minus) sumCount(card, a = -1);
+
+  
+  
 }
 
 function sumCount (card, a) {
@@ -60,7 +62,7 @@ function sumCount (card, a) {
       countBeer+=a;
       let countSpan = card.querySelector('#count');
       countSpan.innerHTML = countBeer;
-      beerCount1 = countBeer;
+      //beerCount1 = countBeer;
   }
   if(countBeer <= 0) {
     countBeer++;
@@ -81,10 +83,8 @@ function beerOrder (beerInOrder, card) {
   let textOrder = document.getElementById('textOrder').innerHTML = '';
   if (arrayBeerInOrder.indexOf(beerInOrder.id) === -1) {
     divOrder = document.querySelector('.order');
-    //console.log(divOrder);
-    let a;
     let beer = `
-        <div id="fd" data-id="${beerInOrder.id}">
+        <div id="beer-order" data-id="${beerInOrder.id}">
           <div class="col-6 col-sm-12 col-md-6 otsp">
             <img src="${beerInOrder.image_url}" class="order-img"/>
           </div>
@@ -93,24 +93,21 @@ function beerOrder (beerInOrder, card) {
             <p>Id - ${beerInOrder.id}</p> 
             <p>Tag Line - ${beerInOrder.tagline}</p>
             <p>Volume - ${beerInOrder.boil_volume.value}</p>
-            <p>Count - ${card.querySelector('#count').innerHTML}</p>
-            <p class="delete-order" data-id="${beerInOrder.id}">Delete order</p>  
+            <p>Count - <span id="count" data-count="${ig}">${card.querySelector('#count').innerHTML}</span></p>
+            <button class="delete-order btn btn-light" data-id="${beerInOrder.id}">Delete order</button> 
           </div>
-          
         </div>
         `;
     divOrder.insertAdjacentHTML('beforeend', beer);
     arrayBeerInOrder.unshift(beerInOrder.id);
-    testAr.unshift({
-      name: beerInOrder.name,
-      id: beerInOrder.id});
+
   } else {
       a = document.querySelector('[data-id="' + beerInOrder.id + '"]');
       console.log(a);
       a.remove();
       let divOrder = document.querySelector('.order');
       let beer = `
-        <div id="fd" data-id="${beerInOrder.id}" class='re'>
+        <div id="beer-order" data-id="${beerInOrder.id}" class='re'>
           <div class="col-6 col-sm-12 col-md-6 otsp">
             <img src="${beerInOrder.image_url}" class="order-img"/>
           </div>
@@ -120,7 +117,7 @@ function beerOrder (beerInOrder, card) {
             <p>Tag Line - ${beerInOrder.tagline}</p>
             <p>Volume - ${beerInOrder.boil_volume.value}</p>
             <p>Count - ${card.querySelector('#count').innerHTML}</p>
-            <p class="delete-order" data-id="${beerInOrder.id}">Delete order</p>
+            <button class="delete-order btn btn-light" data-id="${beerInOrder.id}">Delete order</button>
           </div>
         </div>`;
       divOrder.insertAdjacentHTML('beforeend', beer);
@@ -128,9 +125,10 @@ function beerOrder (beerInOrder, card) {
     let deleteOrder = document.querySelector('.delete-order[data-id="' + beerInOrder.id + '"]');
     //console.log(deleteOrder);
     deleteOrder.onclick = () => {
-      deleteOrder.closest('#fd').remove();
-      if (!divOrder.querySelector('#fd')) {
-        let textOrder = document.getElementById('textOrder').innerHTML = 'Your Shopping Cart is empty';
+      //console.log(deleteOrder.closest('#beer-order'));
+      deleteOrder.closest('#beer-order').remove();
+      if (!divOrder.querySelector('#beer-order')) {
+        document.getElementById('textOrder').innerHTML = 'Your Shopping Cart is empty';
       }
     }
     
@@ -138,3 +136,5 @@ function beerOrder (beerInOrder, card) {
   
 getData();
   
+
+
